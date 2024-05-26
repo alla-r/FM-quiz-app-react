99e48c7a4f5af38b804c9142c226467a8d50e8df
Alla Rakhimova <alla.rakhimova103@gmail.com>
2024-05-26 22:32:42 +0300

    move Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,73 @@
+// import PropTypes from 'prop-types';
+
+import React, { useState, useEffect } from "react";
+import LightSunIcon from "/assets/images/icon-sun-light.svg";
+import DarkSunIcon from "/assets/images/icon-sun-dark.svg";
+import LightMoonIcon from "/assets/images/icon-moon-light.svg";
+import DarkMoonIcon from "/assets/images/icon-moon-dark.svg";
+import Icon from "../Icon";
+import ThemeSwitch from "../ThemeSwitch";
+import styles from "./header.module.css";
+
+function Header({ title, iconConfig }) {
+  const [theme, setTheme] = useState("");
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
+
+  const defaultTheme = () => {
+    const themeLocalStorage = localStorage.getItem("theme");
+    const themeSystem = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+
+    return themeLocalStorage ?? themeSystem;
+  };
+
+  useEffect(() => {
+    if (!theme) return setTheme(defaultTheme());
+
+    document.querySelector(":root").dataset.theme = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <>
+      <header className={styles.header}>
+        {title && (
+          <div className={styles.title}>
+            {iconConfig && <Icon {...iconConfig} />}
+            <h1 className="heading-S">{title}</h1>
+          </div>
+        )}
+
+        <div className={styles.toggle}>
+          <img
+            src={theme === "light" ? DarkSunIcon : LightSunIcon}
+            alt="Light theme icon"
+            width={16}
+            height={16}
+          />
+
+          <ThemeSwitch theme={theme} toggleThemeHandler={toggleTheme} />
+
+          <img
+            src={theme === "light" ? DarkMoonIcon : LightMoonIcon}
+            alt="Light theme icon"
+            width={16}
+            height={16}
+          />
+        </div>
+      </header>
+    </>
+  );
+}
+
+// TODO add prop types
+// Header.defaultProps = { };
+
+// Header.propTypes = {
+// };
+
+export default Header;
