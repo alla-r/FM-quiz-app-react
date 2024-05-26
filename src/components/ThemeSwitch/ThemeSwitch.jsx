d031b3bbc35a68cc093fe01e2ae9ead5c10b21c7
Alla Rakhimova <alla.rakhimova103@gmail.com>
2024-05-26 22:09:21 +0300

    move ThemeSwitch component

---
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -0,0 +1,55 @@
+// import PropTypes from 'prop-types';
+
+import React from "react";
+import Switch from "react-switch";
+import { useMediaQuery } from "react-responsive";
+import styles from "./themeSwitch.module.css";
+
+function ThemeSwitch({ theme, toggleThemeHandler }) {
+  const isTabletOrDesktop = useMediaQuery({ query: "(min-width: 37.5em)" });
+
+  return (
+    <>
+      {isTabletOrDesktop && (
+        <Switch
+          className={styles.switch}
+          onChange={toggleThemeHandler}
+          checked={theme === "dark"}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          width={48}
+          height={28}
+          handleDiameter={20}
+          onColor="#a729f5"
+          offColor="#a729f5"
+          boxShadow="none"
+          activeBoxShadow="none"
+        />
+      )}
+
+      {!isTabletOrDesktop && (
+        <Switch
+          className={styles.switch}
+          onChange={toggleThemeHandler}
+          checked={theme === "dark"}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          width={32}
+          height={20}
+          handleDiameter={12}
+          onColor="#a729f5"
+          offColor="#a729f5"
+          boxShadow="none"
+          activeBoxShadow="none"
+        />
+      )}
+    </>
+  );
+}
+
+// ThemeSwitch.defaultProps = { };
+
+// ThemeSwitch.propTypes = {
+// };
+
+export default ThemeSwitch;
